a335976938369f353caa7efe11085813d232aedf
Mercy <wmercy160@gmail.com>
2025-08-12 20:46:28 +0300

    loops

---
+++ Week_2/functions.py
@@ -0,0 +1,4 @@
+def add(a,b)
+    return a+b
+summation=add (1,2)
+print(summation)
---
+++ Week_2/loops.py
@@ -0,0 +1,9 @@
+i=0
+
+while i<5:
+    print(i)
+    i+=1
+    
+else:
+    print("i is greater than or equal to 5")
+    
