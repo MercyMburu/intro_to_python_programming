f174b0d8f4cec641a214f498d184fd752ff8f3fe
Mercy <wmercy160@gmail.com>
2025-08-12 19:08:08 +0300

    rockpaperscissors game

--- Week_1/day1.py
+++ Week_1/day1.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# Demonstrating variables in python
+
+name= "Mercy"
+# print(name)
+
+name="Python"
+age=30
+height=1.75
+
+# print(name)
+# print(age)
+# print(height)
+
+PI=3.142
+
+print(PI)
+
+# Python Expressions ; values, variables, operators
+
+x=10
+y=5
+z=x+y
+print(z)
+
+greeting="Hello"
+name="Mercy"
+
+print(greeting + " "+ name)
--- Week_1/day2.py
+++ Week_1/day2.py
@@ -1,1 +1,6 @@
-print("Hello World")
+guess=input("Guess a number: ")
+num=10
+while guess!=num:
+    guess=int(input("Enter another number!: " )) 
+print("congratulations")
+
