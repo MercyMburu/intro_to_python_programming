af13f2fcdc225a7e5b6d2184bbaa7fa57744b01c
Mercy <wmercy160@gmail.com>
2025-08-16 14:13:02 +0300

    first commit

--- Week_2/regex.py
+++ Week_2/regex.py
@@ -7,17 +7,17 @@ this is a number 5
 '''
 pattern="come"
 match=re.search(pattern,text)
-# print(match)
+print(match)
 
-# text="We are learning python"
-# pattern=r'^We'
-# matches=re.findall(pattern,text)
-# # print(matches)
+text="We are learning python"
+pattern=r'^We'
+matches=re.findall(pattern,text)
+print(matches)
 
-# text="We are learning python"
-# pattern= r'python$'
-# matches=re.findall(pattern,text)
-# print(matches) 
+text="We are learning python"
+pattern= r'python$'
+matches=re.findall(pattern,text)
+print(matches) 
 
 text=f"""
 My email is user@example.com, or contact@domain.com"""
